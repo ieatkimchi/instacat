ba82a22f50fe7818a8b9592123373492d669fd8d
Robb <kimchibot64@gmail.com>
2018-04-16 17:45:06 -0400

    adds absinthe deps

--- mix.exs
+++ mix.exs
@@ -38,7 +38,10 @@ defmodule Instacat.Mixfile do
       {:phoenix_ecto, "~> 3.2"},
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:absinthe, "~> 1.4.0"},
+      {:absinthe_plug, "~> 1.4"},
+      {:absinthe_ecto, "~> 0.1.3"}
     ]
   end
 
