37bd59d72cc5cc9ab5bc299daf8095f5702bcd36
Robb <kimchibot64@gmail.com>
2018-04-16 21:35:29 -0400

    adds query for single post

--- lib/instacat_web/resolvers/posts.ex
+++ lib/instacat_web/resolvers/posts.ex
@@ -2,4 +2,8 @@ defmodule InstacatWeb.Resolvers.Posts do
   def photos(_, _, _) do
     {:ok, Instacat.Posts.list_photos}
   end
+
+  def photo(_, %{id: id}, _) do
+    {:ok, Instacat.Posts.get_photo!(id)}
+  end
 end
--- lib/instacat_web/schema.ex
+++ lib/instacat_web/schema.ex
@@ -10,5 +10,11 @@ defmodule InstacatWeb.Schema do
     field :photos, list_of(:photo) do
       resolve &Resolvers.Posts.photos/3
     end
+
+    @desc "Gets a photo"
+    field :photo, :photo do
+      arg :id, non_null(:id)
+      resolve &Resolvers.Posts.photo/3
+    end
   end
 end
